ab0b8500b4ae463eb6af23ffc84eb0480007cfd3
abcrd54 <frdahmad54@gmail.com>
2026-04-22 20:55:11 +0700

    fix vercel api imports

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import { config } from "./config";
-import { app } from "./app";
+import { app } from "./app.ts";
 
 app.listen(config.port, () => {
   console.log(`API ready on http://localhost:${config.port}`);
